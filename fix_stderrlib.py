bbd0a0b37bc75378108c4b81ffa0afdb2cb2f0b5
Sheppy <yannik.schmidt@fau.de>
2016-11-02 22:54:57 +0100

    fixed newline

--- fix_stderrlib.py
+++ fix_stderrlib.py
@@ -4,5 +4,5 @@ rc = open("zshrc",'w+')
 old = open("zshrc_old",'r')
 for line in old:
         if line.startswith("LD_PRELOAD="):
-                line = "LD_PRELOAD='"+os.getcwd()+"'"
+                line = "LD_PRELOAD='"+os.getcwd()+"'\n"
         rc.write(line)
