49ce94051027a8c039f188d5ef1dd05769656d6c
Sheppy <yannik.schmidt@fau.de>
2016-11-05 00:32:24 +0100

    some addition scripts

---
+++ other/deb_install.sh
@@ -0,0 +1,9 @@
+apt install build-essential libtool autoconf
+apt install rxvt-unicode zsh xorg herbstluftwm
+apt install git tig python3
+apt install vim vim-nox vim-syntax-gtk
+apt install tree tmux pulseaudio htop sudo ncdu
+apt install iw wicd-curses
+apt install iceweasel chromium libreoffice icedove
+apt install kolourpain feh imagemagick
+apt install kvm sleuthkit
