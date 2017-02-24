2916f380119ae6cd768f7484b6292af28cadec9b
Sheppy <jupiterfisch@web.de>
2017-02-24 16:55:07 +0100

    added pracct deamon

--- herbstluftwm/hl_error.py
+++ herbstluftwm/hl_error.py
@@ -1,6 +1,20 @@
 import datetime
 import sys
 import os
+import subprocess
+import shlex
+import re
+
+def color_remove(s):
+        '''removes colorcodes from inputstring'''
+        return re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]').sub('',s)
+
+def shexec(s):
+        return subprocess.check_output(shlex.split(s)).decode()
+
+def is_cip():
+        u = os.uname()
+        return "cip" in u.release or "faui" in u.name or "ircbox" in u.name
 
 def error(s):
         with open("herbstlog",'a') as f:
---
+++ herbstluftwm/hl_pracct_deamon.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import os
+import sys
+import time
+from hl_error import error, is_cip, shexec, color_remove
+
+#Druckerguthaben
+if is_cip():
+        while(True):
+                out = color_remove(shexec("pr_acct").split("\n")[0]).split(' ')[-1]
+                with open("pracct.log",'w') as f :
+                        f.write(out)
+                time.sleep(60)
+sys.exit()
