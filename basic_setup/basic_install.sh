93cf16ca1874ecc3f8e0b2d34ea2d81b9229ff4c
Sheppy <yannik.schmidt@fau.de>
2017-05-25 16:22:33 +0200

    Added basic install

---
+++ basic_setup/basic_install.sh
@@ -0,0 +1,9 @@
+su
+mv ~/.config/basic_setup/sources.list /etc/apt/sources.list
+apt update
+apt upgrade --asume-yes
+cat ../other/package_list_desktop_essential.txt | while read line
+do
+        apt install --no-install-recommends --assume-yes $line
+done
+exit
