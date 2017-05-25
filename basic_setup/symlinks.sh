292a2a6522a15ea2f7b50d799c555918e1273a65
Sheppy <yannik.schmidt@fau.de>
2017-05-25 23:39:50 +0200

    fixed symlink

--- basic_setup/symlinks.sh
+++ basic_setup/symlinks.sh
@@ -2,4 +2,5 @@ ln -s ~/.config/vim/rc ~/.vimrc
 ln -s ~/.config/zshrc ~/.zshrc
 ln -s ~/.config/xconf/Xressources ~/.Xressources
 ln -s ~/.config/xconf/.xsessionsrc ~/.xinitrc
+ln -s ~/.config/ssh/config ~/.ssh/config
 mkdir -p ~/.zsh
