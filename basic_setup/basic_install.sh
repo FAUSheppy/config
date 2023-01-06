619cfd0719dfabb741b91b933b2f10e48e92cdeb
Yannik Schmidt <sheppy@atlantishq.de>
2023-01-06 19:34:35 +0100

    fix: whitespaces

--- basic_setup/basic_install.sh
+++ basic_setup/basic_install.sh
@@ -2,6 +2,6 @@ apt update
 apt upgrade --asume-yes
 cat ../other/package_list_desktop_essential.txt | while read line
 do
-        apt install --no-install-recommends --assume-yes $line
+    apt install --no-install-recommends --assume-yes $line
 done
 exit
--- basic_setup/full_install.sh
+++ basic_setup/full_install.sh
@@ -4,6 +4,6 @@ apt update
 apt upgrade --asume-yes
 cat ../other/package_list_full.txt | while read line
 do
-        apt install --no-install-recommends --assume-yes $line
+    apt install --no-install-recommends --assume-yes $line
 done
 exit
